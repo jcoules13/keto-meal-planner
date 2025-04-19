eb4a1eaa906ed9058f780dcf001c41358eb6eb7b
jcoules13 <144330373+jcoules13@users.noreply.github.com>
2025-04-19 16:14:22 +0200

    Add files via upload

---
+++ src/components/meals/index.js
@@ -0,0 +1,14 @@
+// Fichier d'index pour faciliter l'importation des composants liés aux repas
+import MacroProgressBar from './MacroProgressBar';
+import WeeklyMealPlanDisplay from './WeeklyMealPlanDisplay';
+import DayMealsList from './DayMealsList';
+import MealItem from './MealItem';
+import NutritionRecalculator from './NutritionRecalculator';
+
+export {
+  MacroProgressBar,
+  WeeklyMealPlanDisplay,
+  DayMealsList,
+  MealItem,
+  NutritionRecalculator
+};
